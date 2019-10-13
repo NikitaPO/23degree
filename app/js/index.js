f3743641371c62dfab920b2b77516bd251d5515e
NikitaPO <gigzim345@gmai.com>
2019-10-13 21:43:44 +0300

    team

--- app/js/index.js
+++ app/js/index.js
@@ -1,3 +1,34 @@
 $(function() {
+  const showSlides = 7;
+  const scrollSlides = 5;
+  const allSlides = $(".slider__item").length;
+  const slider = $(".slider__items");
+
+  slider.slick({
+    infinite: false,
+    slidesToShow: showSlides,
+    slidesToScroll: scrollSlides,
+    appendArrows: false,
+    adaptiveHeight: true
+  })
+
+  // $(slider).on('init reInit',function(event,slick){
+  //   var amount = slick.slideCount;
+  //   $(".slider__range").attr('max',amount);
+  // })
+  //
+  // $(slider).on('afterChange',function(e,slick,currentSlide){
+  //   $(".slider__range").val(currentSlide+1);
+  // })
+  //
+  // $(".slider__range").on('input change',function(){
+  //   $(slider).slick('slickGoTo',this.value-1);
+  // });
+  //
+  // $(slider).slick({
+  //   slidesToShow:2,
+  //   arrows:false,
+  //   dots:false
+  // })
 
 });
